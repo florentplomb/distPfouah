1b1df6dc9679596e7615b35a0ebacdca19c3574b
Gwenaël Magnenat <gwenael.magnenat@heig-vd.ch>
2015-04-22 10:11:32 +0200

    change env to production

--- server/app.js
+++ server/app.js
@@ -6,7 +6,8 @@
 
 
 // Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 var express = require('express');
 var mongoose = require('mongoose');
